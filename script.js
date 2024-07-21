fec21cee09e1f010ce4734c08978ffa1a77063c4
Saidcode2024 <appcode2024@gmail.com>
2024-07-22 00:49:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+var server_filter = ['كتب تهمك','كتب تنمية']
 
-var num=44;
-alert(num);
+var server_urlbook = {}
+
+
+
+server_urlbook[0] = [
+
+    ['data/img/0-0.jpg' , 'أغنى رجل فى بابل' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
+
+    ['data/img/0-1.jpg' , 'جلسات نفسية' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
+
+    ['data/img/0-2.jpg' , 'أرض زيكولا' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
+
+    ['data/img/0-3.jpg' , 'اقفز' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
+
+    ['data/img/0-5.jpg' , 'الفيزياء بين الساطة والذهاء' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],  
+
+] 
+
+server_urlbook[1] = [
+
+
+
+] 
