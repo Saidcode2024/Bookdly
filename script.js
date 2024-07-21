bc24abc73c12382caad203fe886bcab73a968330
Saidcode2024 <appcode2024@gmail.com>
2024-07-22 00:54:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,8 @@ var server_urlbook = {}
 
 server_urlbook[0] = [
 
-    ['data/img/0-0.jpg' , 'أغنى رجل فى بابل' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
-
-    ['data/img/0-1.jpg' , 'جلسات نفسية' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
-
-    ['data/img/0-2.jpg' , 'أرض زيكولا' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
-
-    ['data/img/0-3.jpg' , 'اقفز' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],
-
-    ['data/img/0-5.jpg' , 'الفيزياء بين الساطة والذهاء' , 'AR' , '130' , 'about book' , 'data/pdf.pdf'],  
-
-] 
+    ['15-BTaQ8pAYqMGYVq3cNtfDf1GrLflBws' , 'أغنى رجل فى بابل' , 'AR' , '130' , 'about book' , 'data/pdf.pdf']] 
 
 server_urlbook[1] = [
-
-
 
 ] 
